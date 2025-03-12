f312bb17d36d8399d04738f512aae5746d1f2bfe
SheerazIsmail <sheerazi539@gmail.com>
2025-03-12 20:02:53 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+name = "Sheeraz Ismail"
+age = 14
+is_student = True
+weight = 37.9
+
+print("Name :", name)
+print("Data Type of Name is", type(name))
+
+print("Age", age)
+print("Data Type of Age is", type(age))
+
+print("is_student :", is_student)
+print("Data Type of is_student is", type(is_student))
+
+print("weight :", weight)
+print("Data Type of weight is" , type(weight))
